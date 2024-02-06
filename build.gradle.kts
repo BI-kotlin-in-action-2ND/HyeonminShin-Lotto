5ef04b6048283832026fe6386a14ae2e3009287c
Karatus <shm1193@gmail.com>
2024-02-06 21:04:07 +0900

    update: project setting 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "1.9.21"
 }
 
-group = "kr.bi"
+group = "com.shm"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,12 +10,14 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("io.kotest:kotest-runner-junit5:5.8.0") // kotlin junit 처럼 쓸 수 있는 Spec 들이 정의 됨
+    testImplementation("io.kotest:kotest-assertions-core:5.8.0") // shouldBe... etc 와같이 Assertions 의 기능을 제공
+//    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(19)
 }
