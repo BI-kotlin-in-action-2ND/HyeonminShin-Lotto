1e8d92e31ed62ac0783d8333787715f59b57ab55
Karatus <shm1193@gmail.com>
2024-02-09 01:12:11 +0900

    test: add `MockK` library

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,13 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:5.8.0") // kotlin junit 처럼 쓸 수 있는 Spec 들이 정의 됨
     testImplementation("io.kotest:kotest-assertions-core:5.8.0") // shouldBe... etc 와같이 Assertions 의 기능을 제공
 //    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
+
+    testImplementation("io.mockk:mockk:1.13.9")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 kotlin {
-    jvmToolchain(19)
+    jvmToolchain(17)
 }
