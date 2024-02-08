fc17c375915459dd4608f9ef69195defab4bb611
Karatus <shm1193@gmail.com>
2024-02-09 01:37:56 +0900

    refactor: move folder & add a function doc

--- src/main/kotlin/com/shm/domain/lotto/service/LottoRepositoryService.kt
+++ src/main/kotlin/com/shm/domain/lotto/service/LottoRepositoryService.kt
@@ -1,4 +1,4 @@
-package com.shm.domain.lotto.application
+package com.shm.domain.lotto.service
 
 import com.shm.domain.lotto.dao.LottoRepository
 import com.shm.domain.lotto.dao.MemoryLottoRepository
@@ -6,12 +6,19 @@ import com.shm.domain.lotto.dao.MemoryLottoRepository
 object LottoRepositoryService {
     /**
      * 현재 이용 가능한 메모리 레포지토리를 반환함.
+     *
      * @see MemoryLottoRepository
      */
     fun getLottoRepository() = getMemoryLottoRepository()
 
     private fun getMemoryLottoRepository() = MemoryLottoRepository()
 
+    /**
+     * 주어진 [LottoRepository] 안에 저장된 로또들의 총 당첨 금액을 출력.
+     *
+     * @param target 당첨금 확인용 [LottoRepository]
+     * @return 당첨금 총합
+     */
     fun totalWinningPrize(target: LottoRepository): UInt {
         var total = 0u
 
