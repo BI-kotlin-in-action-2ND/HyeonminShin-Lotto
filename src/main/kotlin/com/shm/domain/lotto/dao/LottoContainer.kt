0859cb7c08121c064cd04366f731f2bd55503f31
Karatus <shm1193@gmail.com>
2024-02-09 01:52:23 +0900

    feat: add toString() from LottoNumber

--- src/main/kotlin/com/shm/domain/lotto/dao/LottoContainer.kt
+++ src/main/kotlin/com/shm/domain/lotto/dao/LottoContainer.kt
@@ -1,24 +1,34 @@
 package com.shm.domain.lotto.dao
 
 import com.shm.domain.lotto.config.LottoRank
+import com.shm.domain.lotto.controller.LottoController
 import com.shm.domain.lotto.domain.Lotto
-import com.shm.domain.lotto.domain.WinningLotto
+import com.shm.global.view.TerminalColor.GREEN
+import com.shm.global.view.TerminalColor.RED
 
 /**
- * [Lotto]와 [LottoRank]를 담는 컨테이너 클래스.
+ * [Lotto]와 [LottoRank]를 담는 data 클래스.
  *
  * @see Lotto
  * @see LottoContainer
- * @see LottoMachine
  */
-class LottoContainer private constructor(val lotto: Lotto, private val rank: LottoRank) : Comparable<LottoContainer> {
+data class LottoContainer(val lotto: Lotto, private val rank: LottoRank) : Comparable<LottoContainer> {
     override fun compareTo(other: LottoContainer): Int {
         return this.rank.ordinal - other.rank.ordinal
     }
 
-    constructor(lotto: Lotto) : this(lotto, LottoRank.getRank(WinningLotto.countMatchedNumbers(lotto)))
+    constructor(lotto: Lotto) : this(lotto, LottoRank.getRank((LottoController.winningLotto.count { it in lotto })))
 
     fun getPrize() = rank.prize
 
-    override fun toString(): String = "${rank.rankString}: $lotto"
+    override fun toString(): String {
+        return "${rank.rankString}: " +
+            lotto.joinToString(" ") {
+                if (it in LottoController.winningLotto) {
+                    GREEN.formatString("%2d".format(it))
+                } else {
+                    RED.formatString("%2d".format(it))
+                }
+            }
+    }
 }
