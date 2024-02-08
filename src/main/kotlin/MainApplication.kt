bad3f6bf6dc9436df77ae3e033ae04a48752cdb7
Karatus <shm1193@gmail.com>
2024-02-09 01:14:13 +0900

    refactor: flattening by deleting LottoStore.kt

--- src/main/kotlin/MainApplication.kt
+++ src/main/kotlin/MainApplication.kt
@@ -1,5 +1,19 @@
-import com.shm.global.api.LottoStore
+import com.shm.domain.user.controller.UserController.createUser
+import com.shm.domain.user.controller.UserController.showUserBoughtLotto
+import com.shm.global.view.dismissPage
+import com.shm.global.view.welcomePage
 
 fun main(args: Array<String>) {
-    LottoStore.open()
+    welcomePage()
+
+    try {
+        val user = createUser()
+        user.buyLotto()
+
+        showUserBoughtLotto(user)
+    } catch (e: Exception) {
+        println(e.message)
+    } finally {
+        dismissPage()
+    }
 }
