af5951b1562f08d2d20be5b8f059aace51353cc0
Karatus <shm1193@gmail.com>
2024-02-13 16:11:15 +0900

    refactor: Int -> LottoNumber

--- src/main/kotlin/com/shm/domain/lotto/domain/generator/RandomLottoGenerator.kt
+++ src/main/kotlin/com/shm/domain/lotto/domain/generator/RandomLottoGenerator.kt
@@ -1,14 +1,12 @@
 package com.shm.domain.lotto.domain.generator
 
-import com.shm.domain.lotto.config.LottoNumberConfig.Companion.MAX_LOTTO_NUMBER
-import com.shm.domain.lotto.config.LottoNumberConfig.Companion.MIN_LOTTO_NUMBER
-import com.shm.domain.lotto.config.LottoNumberConfig.Companion.NUM_OF_LOTTO_NUMBERS
 import com.shm.domain.lotto.domain.Lotto
+import com.shm.domain.lotto.domain.LottoNumber
 
-object RandomLottoGenerator : LottoGenerator {
+class RandomLottoGenerator : LottoGenerator {
     override fun generate() =
         Lotto(
-            (MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER)
-                .shuffled().take(NUM_OF_LOTTO_NUMBERS).toSortedSet(),
+            (LottoNumber.LOTTO_MIN_NUMBER..LottoNumber.LOTTO_MAX_NUMBER)
+                .shuffled().take(Lotto.NUM_OF_LOTTO_NUMBERS).toSortedSet(),
         )
 }
