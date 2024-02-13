d0a331d8886c57c4e6e5f3fc3cc34d7ad4beee19
Karatus <shm1193@gmail.com>
2024-02-13 16:01:08 +0900

    remove: LottoNumberConfig
    각 상수를 필요로 하는 domain 클래스들로 옮김

--- src/main/kotlin/com/shm/domain/lotto/config/LottoNumberConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.shm.domain.lotto.config
-
-class LottoNumberConfig {
-    companion object {
-        const val NUM_OF_LOTTO_NUMBERS = 6
-        const val MIN_LOTTO_NUMBER = 1
-        const val MAX_LOTTO_NUMBER = 45
-    }
-}
